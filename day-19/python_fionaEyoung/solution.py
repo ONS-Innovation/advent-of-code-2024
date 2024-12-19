3d1b665469a1d523ca5bfea55947dc54cc7dcb0e
Fiona Young <fyoung@turing.ac.uk>
2024-12-19 12:16:16 +0000

    Day 19 part 2 (python)

--- day-19/python_fionaEyoung/solution.py
+++ day-19/python_fionaEyoung/solution.py
@@ -1,18 +1,28 @@
 import regex
+from functools import cache
 
-def find_path(graph, start, end):
-    # initialise visit stack with neighbours of start
-    stack = graph[start]
-    visited = [start]
+class StringGraph:
+    def __init__(self, n, edges):
+        self.graph = {i:[] for i in range(n)}
+        for n in edges:
+            self.graph[n[0]].append(n[1])
 
-    while stack:
-        next_node = stack.pop()
-        if next_node is end: return True
-        if not next_node in visited:
-            stack.extend(graph[next_node])
-            visited.append(next_node)
+    # Cache this function, same start+end combos aren't recomputed
+    @cache
+    def find_path(self, start, end):
 
-    return False
+        # initialise visit stack with neighbours of start
+        stack = self.graph[start]
+        found_paths = 0
+
+        while stack:
+            next_node = stack.pop()
+            if next_node is end:
+                found_paths += 1
+                continue
+            found_paths += self.find_path(next_node, end)
+
+        return found_paths
 
 fname = 'input.txt'
 with open(fname, 'r') as f:
@@ -21,17 +31,20 @@ with open(fname, 'r') as f:
     designs = [l.strip() for l in f]
 
 total_possible = 0
+total_combos = 0
 for design in designs:
 
     # Construct graph of matched substrings
-    graph = {i:[] for i in range(len(design))}
+    nodes = []
     for pattern in patterns:
-        nodes = [m.span() for m in regex.finditer(pattern, design, overlapped=True)]
-        for n in nodes:
-            graph[n[0]].append(n[1])
+        nodes.extend([m.span() for m in regex.finditer(pattern, design, overlapped=True)])
+
+    graph = StringGraph(len(design), nodes)
 
     # Is the graph traversable?
-    total_possible += find_path(graph, 0, len(design))
-
+    n = graph.find_path(0, len(design))
+    total_possible += bool(n)
+    total_combos += n
 
 print("Part 1: ", total_possible)
+print("Part 2: ", total_combos)
